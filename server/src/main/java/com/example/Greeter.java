2ff5ec48acfeb3bab65021db2a98a0a9450dc042
Kieron Cairns <kieroncairns@protonmail.com>
2023-02-05 16:13:36 +0000

    Final deployment v11

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,16 @@ public class Greeter {
 
   }
 
+  public static void main( String[] args )
+  {
+    System.out.println("Hello World");
+  }
+
   //TODO: Add javadoc comment
+  // public String greet(String someone) {
+  //   return String.format("Hello, %s!", someone);
+  // }
+
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
